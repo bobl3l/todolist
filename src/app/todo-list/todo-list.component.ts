35399290b37b29ac583698724a83a4861226011c
bobl3l <liangshilinbob@gmail.com>
2024-11-18 16:42:47 +0000

    styled

---
+++ src/app/todo-list/todo-list.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-todo-list',
+  standalone: true,
+  imports: [FormsModule, CommonModule],
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.css'],
+})
+export class TodoListComponent {
+  todos: string[] = [];
+  newTodo: string = '';
+
+  addTodo() {
+    if (this.newTodo.trim()) {
+      this.todos.push(this.newTodo.trim());
+      this.newTodo = '';
+    }
+  }
+
+  removeTodo(index: number) {
+    this.todos.splice(index, 1);
+  }
+}
